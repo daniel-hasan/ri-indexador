e8266de08e692918bef421e27b9f6a482f5b43fe
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-08-09 00:39:47 -0300

    sugestões de melhorias

--- index/indexer.py
+++ index/indexer.py
@@ -6,67 +6,66 @@ import os
 
 
 class Cleaner:
-    def __init__(self,stop_words_file:str,language:str,
-                        perform_stop_words_removal:bool,perform_accents_removal:bool,
-                        perform_stemming:bool):
+    def __init__(self, stop_words_file: str, language: str,
+                 perform_stop_words_removal: bool, perform_accents_removal: bool,
+                 perform_stemming: bool):
         self.set_stop_words = self.read_stop_words(stop_words_file)
 
         self.stemmer = SnowballStemmer(language)
-        in_table =  "áéíóúâêôçãẽõü"
+        in_table = "áéíóúâêôçãẽõü"
         out_table = "aeiouaeocaeou"
-        #altere a linha abaixo para remoção de acentos (Atividade 11)
+        # altere a linha abaixo para remoção de acentos (Atividade 11)
         self.accents_translation_table = None
         self.set_punctuation = set(string.punctuation)
 
-        #flags
+        # flags
         self.perform_stop_words_removal = perform_stop_words_removal
         self.perform_accents_removal = perform_accents_removal
         self.perform_stemming = perform_stemming
 
-    def html_to_plain_text(self,html_doc:str) ->str:
+    def html_to_plain_text(self, html_doc: str) -> str:
         return None
 
-    def read_stop_words(self,str_file):
+    @staticmethod
+    def read_stop_words(str_file) -> set:
         set_stop_words = set()
-        with open(str_file, "r") as stop_words_file:
+        with open(str_file, encoding='utf-8') as stop_words_file:
             for line in stop_words_file:
                 arr_words = line.split(",")
                 [set_stop_words.add(word) for word in arr_words]
         return set_stop_words
-    def is_stop_word(self,term:str):
+
+    def is_stop_word(self, term: str):
         return True
 
-    def word_stem(self,term:str):
+    def word_stem(self, term: str):
         return ""
 
-
-    def remove_accents(self,term:str) ->str:
+    def remove_accents(self, term: str) -> str:
         return None
 
-
-    def preprocess_word(self,term:str) -> str:
-
+    def preprocess_word(self, term: str) -> str or None:
         return None
-
 
 
 class HTMLIndexer:
     cleaner = Cleaner(stop_words_file="stopwords.txt",
-                        language="portuguese",
-                        perform_stop_words_removal=True,
-                        perform_accents_removal=True,
-                        perform_stemming=True)
-    def __init__(self,index):
+                      language="portuguese",
+                      perform_stop_words_removal=True,
+                      perform_accents_removal=True,
+                      perform_stemming=True)
+
+    def __init__(self, index):
         self.index = index
 
-    def text_word_count(self,plain_text:str):
+    def text_word_count(self, plain_text: str):
         dic_word_count = {}
 
         return dic_word_count
-    def index_text(self,doc_id:int, text_html:str):
+
+    def index_text(self, doc_id: int, text_html: str):
         pass
 
-    def index_text_dir(self,path:str):
-
+    def index_text_dir(self, path: str):
         for str_sub_dir in os.listdir(path):
             path_sub_dir = f"{path}/{str_sub_dir}"
--- index/performance_test.py
+++ index/performance_test.py
@@ -5,11 +5,7 @@ from datetime import datetime
 import math
 import tracemalloc
 import unittest
-from random import randrange,seed
-
-
-
-
+from random import randrange, seed
 
 
 class PerformanceTest(unittest.TestCase):
@@ -21,20 +17,20 @@ class PerformanceTest(unittest.TestCase):
 
     def create_vocabulary(self):
         vocabulary = []
-        for i in range(65,91):
-            for j in range(65,91):
-                for k in range(65,91):
+        for i in range(65, 91):
+            for j in range(65, 91):
+                for k in range(65, 91):
                     vocabulary.append(f"{chr(i)}{chr(j)}{chr(k)}")
         return vocabulary
 
-    def print_status(self,count:int,total:int):
+    def print_status(self, count: int, total: int):
 
-        delta = datetime.now()-self.time
-        porc_complete = math.floor(count/total*100)
+        delta = datetime.now() - self.time
+        porc_complete = math.floor(count / total * 100)
         current, peak = tracemalloc.get_traced_memory()
 
         clear_output(wait=True)
-        print(f"Memoria usada: {current / 10**6:,} MB; Máximo {peak / 10**6:,} MB")
+        print(f"Memoria usada: {current / 10 ** 6:,} MB; Máximo {peak / 10 ** 6:,} MB")
         print(f"Indexando ocorrencia #{count:,}/{total:,} ({porc_complete}%)")
         print(f"Tempo gasto: {delta.total_seconds()}s")
 
@@ -44,18 +40,17 @@ class PerformanceTest(unittest.TestCase):
         words = []
         for doc_i in range(PerformanceTest.NUM_DOCS):
             for term_j in range(PerformanceTest.NUM_TERM_PER_DOC):
-                idx_term = randrange(0,len(self.vocabulary))
+                idx_term = randrange(0, len(self.vocabulary))
                 str_term = self.vocabulary[idx_term]
-                self.index.index(str_term, doc_i, (count%10)+1)
-                #indiceTeste.index(vocabulario[(count+1)%15625], d, (count%10)+1);
-                if count%50000==0:
-                    self.print_status(count,PerformanceTest.NUM_DOCS*PerformanceTest.NUM_TERM_PER_DOC)
+                self.index.index(str_term, doc_i, (count % 10) + 1)
+                # indiceTeste.index(vocabulario[(count+1)%15625], d, (count%10)+1);
+                if count % 50000 == 0:
+                    self.print_status(count, PerformanceTest.NUM_DOCS * PerformanceTest.NUM_TERM_PER_DOC)
 
-                count+=1
+                count += 1
         return count
 
     def test_performance(self):
-
 
         print("Criando vocabulário...")
         self.vocabulary = self.create_vocabulary()
@@ -63,19 +58,24 @@ class PerformanceTest(unittest.TestCase):
         tracemalloc.start()
         self.time = datetime.now()
         total = self.index_words()
-        self.print_status(total,total)
+        self.print_status(total, total)
         tracemalloc.stop()
 
+
 import time
+
+
 class FilePerformanceTest(PerformanceTest):
     def setUp(self):
         self.index = FileIndex()
+
 
 def test():
     for i in range(10):
         clear_output(wait=True)
         print(f"oi {i}")
         time.sleep(1)
+
 
 if __name__ == "__main__":
     unittest.main()
